59494c2fc242d782baa4e6c37c372215f794bb9c
ramritheesh <128439526+ramritheesh@users.noreply.github.com>
2025-12-14 19:58:14 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
-export default {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // Ensures assets are loaded relative to index.html
+})
